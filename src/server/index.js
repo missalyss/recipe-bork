cf7475405c0b371e8d1327b9ee5e1c82e41bda0d
aevans <aevans@starbucks.com>
2020-06-13 16:33:15 -0700

    almost created a server for chef speak, but would need to pay a monthly. saved code in comments in case i want the shortcut

--- src/server/index.js
+++ src/server/index.js
@@ -1,3 +1,32 @@
+// THIS IS A SAMPLE SERVER. NPM I --SAVE EXPRESS AND NODEMON IF YOU END UP NEEDING A SERVER.
+// const express = require('express');
+// const bodyParser = require('body-parser')
+// const path = require('path');
+// const app = express();
+// app.use(express.static(path.join(__dirname, 'build')));
+//
+// app.get('/ping', (req, res) => {
+//  return res.send('pong');
+// });
+//
+// app.get('/', (req, res) => {
+  // THIS DOESN'T WORK BUT OH WELL.
+//   res.sendFile(path.join(__dirname, 'build', 'index.html'));
+// });
+
+// const port = process.env.PORT || 8080;
+// app.listen(port, () => console.log(`Listening on port ${port}`));
+
+// NOT FUNCTIONING POST.
+// app.post('https://api.funtranslations.com/translate/chef.json?text=today we will have soup', (req, res) => {
+//     console.log('req dot body! ', req.body);
+//   res.send(
+//     `I received your POST request. This is what you sent me: ${req.body.post}`,
+//   );
+// })
+
+
+// THIS IS A CLIENTSIDE FETCH
 // fetch('https://api.funtranslations.com/translate/chef.json')
 //   .then(response => response.json())
 //   .then(data => {
@@ -5,19 +34,16 @@
 //   })
 //   .catch(error => console.error(error))
 
-
-postRequest('https://api.funtranslations.com/translate/chef.json', { text: 'Krunal' })
-  .then(data => console.log(data)) // Result from the `response.json()` call
-  .catch(error => console.error(error))
-
-function postRequest(url, data) {
-  return fetch(url, {
-    credentials: 'same-origin', // 'include', default: 'omit'
-    method: 'POST', // 'GET', 'PUT', 'DELETE', etc.
-    body: JSON.stringify(data), // Coordinate the body type with 'Content-Type'
-    headers: new Headers({
-      'Content-Type': 'application/json'
-    }),
-  });
-  .then(response => response.json())
-}
+// THIS ALSO IS PROBABLY A CLIENTSIDE FETCH
+// const postRequest = ( { url ='https://api.funtranslations.com/translate/chef.json', text = 'Hello we are going to make soup today' }) => {
+//   return fetch(url, {
+//     credentials: 'same-origin', // 'include', default: 'omit'
+//     method: 'POST', // 'GET', 'PUT', 'DELETE', etc.
+//     body: JSON.stringify(text), // Coordinate the body type with 'Content-Type'
+//     headers: new Headers({
+//       'Content-Type': 'application/json'
+//     }),
+//   })
+//   .then(response => response.json())
+//   .catch(err => console.error('RED ALERT: ', err))
+// }
