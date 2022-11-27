b8615c5ee2b5b8e2689b7e80a16bec43b6f97c24
aevans <aevans@starbucks.com>
2022-11-27 01:02:48 -0800

    lint and bork

--- src/components/sign-in.js
+++ src/components/sign-in.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 
-const CURRENT_PASSWORD = "primedirective"
+const CURRENT_PASSWORD = "primedirective";
 
 const SignIn = ({ isBorked }) => {
   const [inputVal, setInputVal] = useState("");
@@ -13,15 +13,15 @@ const SignIn = ({ isBorked }) => {
   };
   return (
     <React.Fragment>
-        <input
-          type="password"
-          value={inputVal}
-          onChange={(e) => setInputVal(e.target.value)}
-          placeholder={isBorked ? "Serkrit Parsewerd" : "Secret Password"}
-        />
-        <button type="button" onClick={signIn}>
-          {isBorked ? "Lerg Ine" : "Log In"}
-        </button>
+      <input
+        type="password"
+        value={inputVal}
+        onChange={(e) => setInputVal(e.target.value)}
+        placeholder={isBorked ? "Serkrit Parsewerd" : "Secret Password"}
+      />
+      <button type="button" onClick={signIn}>
+        {isBorked ? "Lug In" : "Log In"}
+      </button>
     </React.Fragment>
   );
 };
